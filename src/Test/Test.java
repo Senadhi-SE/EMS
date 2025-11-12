85558b1ae6260a7c6df373a512dc730120de05e0
Senadhi-SE <senadhimayadunna03@gmail.com>
2025-11-12 16:28:05 +0530

    ui building half done

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -2,18 +2,20 @@ package Test;
 
 import model.SystemManager;
 import java.sql.ResultSet;
+import model.BarcodeGenerator;
 
 public class Test {
-    
+
     public static void main(String[] args) {
         try {
-//            ResultSet result = SystemManager.salaryManager.getSalaries("909876543V");
-//            while (result.next()) {
-//                System.out.println(result.getString("fname") + " " + result.getString("month") + " " + result.getString("paid_amount"));
-//            }
-              System.out.println(SystemManager.employeeManager.createEmployee(19, "saman", "dissanayake", "200327311409", "Male", "maya@gmail.com", "0784544285", "matara"));
-        } catch (Exception ex) {
-            ex.printStackTrace();
+            /*            
+            Steps to create an employee
+            SystemManager -> employeeManager -> .createEmployee(....)
+            */
+            
+            //following line prints "success" if that operation is successful
+            System.out.println(SystemManager.employeeManager.createEmployee(18, "hhhhh", "ddddd", "200327319410", "Female", "sithmi@gmail.com", "0784544285", "Thalalla"));
+        } catch (Exception e) {
         }
-    }    
+    }
 }
